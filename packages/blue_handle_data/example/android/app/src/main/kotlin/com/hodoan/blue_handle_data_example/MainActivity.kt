513c9fcdfbd5fc2d20cae427bc750ec5ebda4329
DoanHo <doanho.it.dev@gmail.com>
2022-11-10 15:28:09 +0700

    remove-example-blue_handle_data

--- packages/blue_handle_data/example/android/app/src/main/kotlin/com/hodoan/blue_handle_data_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.hodoan.blue_handle_data_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- packages/blue_handle_data/lib/blue_handle_data.dart
+++ packages/blue_handle_data/lib/blue_handle_data.dart
@@ -1,8 +1,3 @@
-
-import 'blue_handle_data_platform_interface.dart';
-
-class BlueHandleData {
-  Future<String?> getPlatformVersion() {
-    return BlueHandleDataPlatform.instance.getPlatformVersion();
-  }
-}
+export 'core/handle_data.dart';
+export 'models/glucose.dart';
+export 'models/metro_info.dart';
